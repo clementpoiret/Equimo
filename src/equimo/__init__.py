e91af267b09c94ea303fbb75c8a1852f27f1aca5
Clément POIRET <clement@linux.com>
2026-01-13 14:15:55 +0100

    fix(models): missing imports

--- src/equimo/__init__.py
+++ src/equimo/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "0.5.0-alpha.15"
+__version__ = "0.5.0-alpha.16"
--- src/equimo/models/__init__.py
+++ src/equimo/models/__init__.py
@@ -1,4 +1,5 @@
 from .fastervit import FasterViT
+from .iformer import IFormer, iformer_t, iformer_s, iformer_m, iformer_l
 from .mlla import Mlla
 from .mobilenet import MobileNetv3, mobilenetv3_large, mobilenetv3_small
 from .partialformer import PartialFormer
